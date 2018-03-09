5501fb2b9fffb1c8fe63d75f82ae966c9f263975
Kat Rollo <rollo.katherine@gmail.com>
2018-03-09 20:20:28 +0800

    Add TODO for testng.xml

--- src/test/java/jcucumberng/runners/MainRunner.java
+++ src/test/java/jcucumberng/runners/MainRunner.java
@@ -15,6 +15,7 @@ public class MainRunner extends AbstractTestNGCucumberTests {
 // Right-Click > Run As > TestNG Test
 
 // TODO: Implement Log4j 2
+// TODO: Add testng.xml
 // TODO: Update to Cucumber 2.x
 // TODO: Integrate with Selenium Grid
 // TODO: Integrate with Jenkins
