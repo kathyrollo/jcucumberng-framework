62d43f14fef946e61c3c9f43cfeca16e069e2f43
Kat Rollo <rollo.katherine@gmail.com>
2018-03-10 16:31:51 +0800

    Change from ~@tag to not @tag in @CucumberOptions

--- src/test/java/jcucumberng/runners/MainRunner.java
+++ src/test/java/jcucumberng/runners/MainRunner.java
@@ -4,10 +4,11 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = "src/test/resources/features", tags = { "~@ignore" }, glue = "jcucumberng/steps", plugin = {
-		"pretty", "html:target/cucumber-generated-reports/cucumber-report",
-		"json:target/cucumber-generated-reports/cucumber-report.json",
-		"junit:target/cucumber-generated-reports/cucumber-report.xml" }, snippets = SnippetType.UNDERSCORE, monochrome = true, dryRun = false)
+@CucumberOptions(features = "src/test/resources/features", tags = {
+		"not @ignore" }, glue = "jcucumberng/steps", plugin = { "pretty",
+				"html:target/cucumber-generated-reports/cucumber-report",
+				"json:target/cucumber-generated-reports/cucumber-report.json",
+				"junit:target/cucumber-generated-reports/cucumber-report.xml" }, snippets = SnippetType.UNDERSCORE, monochrome = true, dryRun = false)
 
 public class MainRunner extends AbstractTestNGCucumberTests {
 }
