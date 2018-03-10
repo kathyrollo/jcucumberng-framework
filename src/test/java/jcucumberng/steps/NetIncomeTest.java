4c96d94d5b4cceb1ad42594ba4821ee04e6dd464
Kat Rollo <rollo.katherine@gmail.com>
2018-03-10 18:47:34 +0800

    Fix imports

--- src/test/java/jcucumberng/steps/NetIncomeTest.java
+++ src/test/java/jcucumberng/steps/NetIncomeTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
-//import org.testng.Assert;
 import org.testng.Assert;
 
 import com.paulhammant.ngwebdriver.ByAngular;
