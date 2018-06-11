a66e195f87ab7bcf2cdb6d525fdabfeffea7a447
Kat Rollo <rollo.katherine@gmail.com>
2018-06-11 17:45:59 +0800

    Refactor CucumberRunner

--- src/test/java/jcucumberng/runners/CucumberRunner.java
+++ src/test/java/jcucumberng/runners/CucumberRunner.java
@@ -4,8 +4,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = { "src/test/resources/features" }, tags = { "@netPerMonth" }, glue = {
-		"jcucumberng/steps/defs", "jcucumberng/steps/config", "jcucumberng/steps/hooks" }, plugin = { "pretty",
+@CucumberOptions(features = { "src/test/resources/features" }, tags = { "not @ignore" }, glue = {
+		"jcucumberng/steps/config", "jcucumberng/steps/defs", "jcucumberng/steps/hooks" }, plugin = { "pretty",
 				"html:target/cucumber-generated-reports/cucumber-report",
 				"json:target/cucumber-generated-reports/cucumber-report.json",
 				"junit:target/cucumber-generated-reports/cucumber-report.xml" }, snippets = SnippetType.UNDERSCORE, monochrome = true, strict = true, dryRun = false)
