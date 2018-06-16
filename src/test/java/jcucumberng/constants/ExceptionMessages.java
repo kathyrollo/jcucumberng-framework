0fe4746fb38109277ae7110f3f038df9ca840e80
Kat Rollo <rollo.katherine@gmail.com>
2018-06-16 08:24:25 +0800

    Change ExceptionMessages to abstract class

--- src/test/java/jcucumberng/constants/ExceptionMessages.java
+++ src/test/java/jcucumberng/constants/ExceptionMessages.java
@@ -1,5 +1,5 @@
 package jcucumberng.constants;
 
-public class ExceptionMessages {
+public abstract class ExceptionMessages {
 	public static final String MISSING_ARGS = "No arguments found for arbitrary parameters. Length must not be 0.";
 }
