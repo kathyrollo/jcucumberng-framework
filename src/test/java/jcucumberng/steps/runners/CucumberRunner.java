81f6775ff5113504f1ddebfd745a5c58d714541d
Kat Rollo <rollo.katherine@gmail.com>
2018-06-22 00:30:41 +0800

    Refactor to test-report

--- src/test/java/jcucumberng/steps/runners/CucumberRunner.java
+++ src/test/java/jcucumberng/steps/runners/CucumberRunner.java
@@ -6,8 +6,8 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(features = { "src/test/resources/features" }, tags = { "not @ignore" }, glue = {
 		"jcucumberng/steps/config", "jcucumberng/steps/defs", "jcucumberng/steps/hooks" }, plugin = { "pretty",
-				"html:target/cucumber-output/report", "json:target/cucumber-output/report.json",
-				"junit:target/cucumber-output/report.xml" }, snippets = SnippetType.UNDERSCORE, monochrome = true, strict = true, dryRun = false)
+				"html:target/cucumber-output/test-report", "json:target/cucumber-output/test-report.json",
+				"junit:target/cucumber-output/test-report.xml" }, snippets = SnippetType.UNDERSCORE, monochrome = true, strict = true, dryRun = false)
 
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 }
