a5667fe7be83a48a68525b9c1cf67ef1f677b3e2
Kat Rollo <rollo.katherine@gmail.com>
2018-06-22 15:30:04 +0800

    Remove main() from App.java

--- src/main/java/jcucumberng/app/App.java
+++ src/main/java/jcucumberng/app/App.java
@@ -2,11 +2,6 @@ package jcucumberng.app;
 
 public class App {
 
-	public static void main(String[] args) {
-		/*
-		 * This class prevents the [WARNING] message for mvn clean install because it
-		 * looks for a class to put in the jar.
-		 */
-	}
+	// This is a sample app.
 
 }
