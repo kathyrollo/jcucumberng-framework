b67874ef18bcec2b7fae434138bae744bc732d32
Kat Rollo <rollo.katherine@gmail.com>
2018-06-28 22:24:08 +0800

    Implement NoSuchKeyException

--- src/test/java/jcucumberng/framework/strings/Messages.java
+++ src/test/java/jcucumberng/framework/strings/Messages.java
@@ -1,6 +1,8 @@
 package jcucumberng.framework.strings;
 
 public abstract class Messages {
-	public static final String UNSUPPORTED_BROWSER = "Unsupported browser specified in config.properties. Using default.";
 	public static final String MISSING_ARGS = "No arguments found for arbitrary parameters. Length must not be equal to 0.";
+	public static final String NO_SUCH_KEY_CONFIG = "Key not found in config.properties file: ";
+	public static final String NO_SUCH_KEY_UIMAP = "Key not found in ui-map.properties file: ";
+	public static final String UNSUPPORTED_BROWSER = "Unsupported browser specified in config.properties. Using default.";
 }
