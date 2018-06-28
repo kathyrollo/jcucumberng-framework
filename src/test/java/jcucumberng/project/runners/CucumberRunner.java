39a25373c4d2dbf8b29ac1d1a49fd96239cd6ee1
Kat Rollo <rollo.katherine@gmail.com>
2018-06-28 22:00:22 +0800

    Revert to cucumber-junit

--- src/test/java/jcucumberng/project/runners/CucumberRunner.java
+++ src/test/java/jcucumberng/project/runners/CucumberRunner.java
@@ -1,20 +1,20 @@
 package jcucumberng.project.runners;
 
+import org.junit.runner.RunWith;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
+import cucumber.api.junit.Cucumber;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(features = { "src/test/resources/features" }, tags = { "not @ignore" }, glue = {
 		"jcucumberng/project/typeregistry", "jcucumberng/project/stepdefs", "jcucumberng/project/hooks" }, plugin = {
 				"pretty", "html:target/jcucumberng-output/cucumber-html-default",
 				"json:target/jcucumberng-output/test-report.json",
 				"junit:target/jcucumberng-output/test-report.xml" }, snippets = SnippetType.UNDERSCORE, monochrome = true, strict = true, dryRun = false)
 
-public class CucumberRunner extends AbstractTestNGCucumberTests {
+public class CucumberRunner {
 }
 
-// For Eclipse IDE: Right-Click > Run As > TestNG Test
-
 // TODO Implement NoSuchKeyException
-// TODO Revert to cucumber-junit
 // TODO Add other by-types to Selenium.by(String)
