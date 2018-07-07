1a5b5ab8705a00875f2774b7631b61512098210b
Kat Rollo <rollo.katherine@gmail.com>
2018-07-07 18:06:21 +0800

    Move log4j2.xml to resources/jcucumberng/framework

--- src/test/java/jcucumberng/project/hooks/ScenarioHook.java
+++ src/test/java/jcucumberng/project/hooks/ScenarioHook.java
@@ -1,5 +1,10 @@
 package jcucumberng.project.hooks;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
@@ -10,9 +15,26 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import jcucumberng.framework.api.LocalMachine;
 import jcucumberng.framework.api.PropsLoader;
+import jcucumberng.framework.exceptions.LoggerConfigException;
 import jcucumberng.framework.factory.BrowserFactory;
+import jcucumberng.framework.strings.Messages;
 
 public class ScenarioHook {
+	// Set logger config
+	static {
+		StringBuilder builder = new StringBuilder();
+		builder.append(System.getProperty("user.dir").replace("\\", "/"));
+		builder.append("/src/test/resources/jcucumberng/framework/log4j2.xml");
+
+		try {
+			InputStream inputStream = new FileInputStream(builder.toString());
+			ConfigurationSource source = new ConfigurationSource(inputStream);
+			Configurator.initialize(null, source);
+		} catch (Exception ex) {
+			throw new LoggerConfigException(Messages.LOGGER_CONFIG_FAIL);
+		}
+	}
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(ScenarioHook.class);
 	private Scenario scenario = null;
 	private WebDriver driver = null;
