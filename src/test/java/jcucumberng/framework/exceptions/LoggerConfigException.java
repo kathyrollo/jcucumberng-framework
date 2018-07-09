0da3e9fde4a703f3a7187ae26fb982cb372864da
Kat Rollo <rollo.katherine@gmail.com>
2018-07-09 09:40:42 +0800

    Update javadocs for custom exceptions

--- src/test/java/jcucumberng/framework/exceptions/LoggerConfigException.java
+++ src/test/java/jcucumberng/framework/exceptions/LoggerConfigException.java
@@ -2,7 +2,7 @@ package jcucumberng.framework.exceptions;
 
 /**
  * {@code LoggerConfigException} is thrown when an exception is encountered
- * while loading {@code log4j2.xml} from a specified directory.
+ * while loading the log4j2 config file from a specified directory.
  * 
  * @author Kat Rollo <rollo.katherine@gmail.com>
  */
