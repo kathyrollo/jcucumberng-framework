8512ebfa03bee081a3aff0400fe26d6e6e290980
Kat Rollo <rollo.katherine@gmail.com>
2018-07-24 23:03:25 +0800

    Rename CucumberRunner to BalanceProjectorRunner Replace glue with package names

--- src/test/java/jcucumberng/project/runners/BalanceProjectorRunner.java
+++ src/test/java/jcucumberng/project/runners/BalanceProjectorRunner.java
@@ -10,11 +10,11 @@ import jcucumberng.framework.api.ConfigLoader;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "src/test/resources/jcucumberng/project/features" }, tags = { "not @ignore" }, glue = {
-		"jcucumberng/project/typeregistry", "jcucumberng/project/stepdefs", "jcucumberng/project/hooks" }, plugin = {
+		"jcucumberng.project.typeregistry", "jcucumberng.project.stepdefs", "jcucumberng.project.hooks" }, plugin = {
 				"pretty", "html:target/cucumber-html-default", "json:target/cucumber-report.json",
 				"junit:target/cucumber-report.xml" }, snippets = SnippetType.UNDERSCORE, monochrome = true, strict = true, dryRun = false)
 
-public class CucumberRunner {
+public class BalanceProjectorRunner {
 	private static boolean isLoaded = false;
 
 	@BeforeClass
