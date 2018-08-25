a93e2d9b75b02dab75b847885343e24ebd485922
Kat Rollo <rollo.katherine@gmail.com>
2018-08-25 09:57:14 +0800

    Refactor NetIncomeProjectorSteps

--- src/test/java/jcucumberng/project/stepdefs/NetIncomeProjectorSteps.java
+++ src/test/java/jcucumberng/project/stepdefs/NetIncomeProjectorSteps.java
@@ -3,7 +3,6 @@ package jcucumberng.project.stepdefs;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.slf4j.Logger;
@@ -18,16 +17,16 @@ import jcucumberng.project.hooks.ScenarioHook;
 
 public class NetIncomeProjectorSteps {
 	private static final Logger LOGGER = LoggerFactory.getLogger(NetIncomeProjectorSteps.class);
-	private WebDriver driver = null;
+	private Selenium selenium = null;
 
 	// PicoContainer injects ScenarioHook class
 	public NetIncomeProjectorSteps(ScenarioHook scenarioHook) {
-		driver = scenarioHook.getDriver();
+		selenium = scenarioHook.getSelenium();
 	}
 
 	@When("I Enter My Start Balance: {word}")
 	public void I_Enter_My_Start_Balance(String value) throws Throwable {
-		Selenium.type(driver, value, "start.balance");
+		selenium.type(value, "start.balance");
 		LOGGER.debug("Start Balance=" + value);
 		this.scrollToDivBox(0);
 	}
@@ -49,43 +48,43 @@ public class NetIncomeProjectorSteps {
 
 	@Then("I Should See Net Income Per Month: {word}")
 	public void I_Should_See_Net_Income_Per_Month(String expected) throws Throwable {
-		WebElement netPerMonth = Selenium.getVisibleElement(driver, "net.per.month");
+		WebElement netPerMonth = selenium.getVisibleElement("net.per.month");
 		String actual = netPerMonth.getText();
 		Assertions.assertThat(actual).isEqualTo(expected);
 		LOGGER.debug("Net Per Month=" + actual);
-		Selenium.scrollToElement(driver, netPerMonth);
+		selenium.scrollToElement(netPerMonth);
 	}
 
 	@Then("I Should See Net Income Per Year: {word}")
 	public void I_Should_See_Net_Income_Per_Year(String expected) throws Throwable {
-		WebElement netPerYear = Selenium.getVisibleElement(driver, "net.per.year");
+		WebElement netPerYear = selenium.getVisibleElement("net.per.year");
 		String actual = netPerYear.getText();
 		Assertions.assertThat(actual).isEqualTo(expected);
 		LOGGER.debug("Net Per Year=" + actual);
-		Selenium.scrollToElement(driver, netPerYear);
+		selenium.scrollToElement(netPerYear);
 	}
 
 	private void enterTransaction(List<Transaction> txns, String add, String name, String amount, String freq)
 			throws Throwable {
 		// Click Add button
 		for (int ctr = 0; ctr < txns.size() - 1; ctr++) {
-			Selenium.click(driver, add);
+			selenium.click(add);
 		}
 		// Enter details
-		List<WebElement> names = Selenium.getVisibleElements(driver, name);
-		List<WebElement> amounts = Selenium.getVisibleElements(driver, amount);
-		List<Select> freqs = Selenium.getSelectElements(driver, freq);
+		List<WebElement> names = selenium.getVisibleElements(name);
+		List<WebElement> amounts = selenium.getVisibleElements(amount);
+		List<Select> freqs = selenium.getSelectElements(freq);
 		for (int ctr = 0; ctr < txns.size(); ctr++) {
-			Selenium.type(driver, txns.get(ctr).getName(), names.get(ctr));
-			Selenium.type(driver, txns.get(ctr).getAmount(), amounts.get(ctr));
+			selenium.type(txns.get(ctr).getName(), names.get(ctr));
+			selenium.type(txns.get(ctr).getAmount(), amounts.get(ctr));
 			freqs.get(ctr).selectByVisibleText(txns.get(ctr).getFrequency());
 			LOGGER.debug(txns.get(ctr).toString());
 		}
 	}
 
 	private void scrollToDivBox(int index) throws Throwable {
-		List<WebElement> divBoxes = Selenium.getVisibleElements(driver, "div.boxes");
-		Selenium.scrollToElement(driver, divBoxes.get(index));
+		List<WebElement> divBoxes = selenium.getVisibleElements("div.boxes");
+		selenium.scrollToElement(divBoxes.get(index));
 	}
 
 }
