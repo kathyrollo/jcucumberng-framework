e72005c2eaa73d5a5f1eeb661b29c27008fffb7d
Kat Rollo <rollo.katherine@gmail.com>
2018-08-26 09:57:48 +0800

    Refactor datatable

--- src/test/java/jcucumberng/project/RunCukesTest.java
+++ src/test/java/jcucumberng/project/RunCukesTest.java
@@ -10,7 +10,7 @@ import jcucumberng.framework.utils.Config;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "src/test/resources/jcucumberng/project/features" }, tags = { "not @ignore" }, glue = {
-		"jcucumberng.project.typeregistry", "jcucumberng.project.stepdefs", "jcucumberng.project.hooks" }, plugin = {
+		"jcucumberng.project.datatable", "jcucumberng.project.stepdefs", "jcucumberng.project.hooks" }, plugin = {
 				"pretty", "html:target/cucumber-html-default", "json:target/cucumber-report.json",
 				"junit:target/cucumber-report.xml" }, snippets = SnippetType.UNDERSCORE, monochrome = true, strict = true, dryRun = false)
 
