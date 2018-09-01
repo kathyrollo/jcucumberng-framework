0554734d295e4512afe292551cc5c22639e10ba0
Kat Rollo <rollo.katherine@gmail.com>
2018-09-01 22:41:02 +0800

    Rename dir to cucumber-html for default report

--- src/test/java/runners/RunCukesTest.java
+++ src/test/java/runners/RunCukesTest.java
@@ -12,7 +12,7 @@ import jcucumberng.api.logger.Logger;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "src/main/resources/features" }, tags = { "not @ignore" }, glue = { "project.datatable",
 		"project.hooks", "project.stepdefs" }, plugin = { "pretty", "io.qameta.allure.cucumber3jvm.AllureCucumber3Jvm",
-				"html:target/cucumber-html-default", "json:target/cucumber-report.json",
+				"html:target/cucumber-html", "json:target/cucumber-report.json",
 				"junit:target/cucumber-report.xml" }, snippets = SnippetType.UNDERSCORE, monochrome = true, strict = true, dryRun = false)
 
 public class RunCukesTest {
