60cbd6078fcba6650c86b4d7c266369d96661ff7
Kat Rollo <rollo.katherine@gmail.com>
2018-09-01 21:40:00 +0800

    Replace src/test to src/main

--- src/test/java/project/RunCukesTest.java
+++ src/test/java/project/RunCukesTest.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 import jcucumberng.api.logger.Logger;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/resources/features" }, tags = { "not @ignore" }, glue = { "project.datatable",
+@CucumberOptions(features = { "src/main/resources/features" }, tags = { "not @ignore" }, glue = { "project.datatable",
 		"project.hooks", "project.stepdefs" }, plugin = { "pretty", "io.qameta.allure.cucumber3jvm.AllureCucumber3Jvm",
 				"html:target/cucumber-html-default", "json:target/cucumber-report.json",
 				"junit:target/cucumber-report.xml" }, snippets = SnippetType.UNDERSCORE, monochrome = true, strict = true, dryRun = false)
