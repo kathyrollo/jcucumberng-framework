b0f8eb390ec5ac769fd33df5e7ec5f470a74d524
Kat Rollo <rollo.katherine@gmail.com>
2018-09-02 19:00:24 +0800

    Fix code smells in SystemUtil

--- src/main/java/jcucumberng/api/utils/SystemUtil.java
+++ src/main/java/jcucumberng/api/utils/SystemUtil.java
@@ -30,8 +30,7 @@ public final class SystemUtil {
 		java.awt.Dimension awtDimension = Toolkit.getDefaultToolkit().getScreenSize();
 		short width = (short) awtDimension.getWidth();
 		short height = (short) awtDimension.getHeight();
-		Dimension dimension = new Dimension(width, height);
-		return dimension;
+		return new Dimension(width, height);
 	}
 
 	/**
@@ -39,17 +38,15 @@ public final class SystemUtil {
 	 * 
 	 * @param key the constant from {@code java.awt.event.KeyEvent}
 	 * @throws AWTException
-	 * @throws NumberFormatException
 	 * @throws IOException
 	 */
-	public static void pressKey(int key) throws AWTException, NumberFormatException, IOException {
+	public static void pressKey(int key) throws AWTException, IOException {
 		Robot robot = new Robot();
 		robot.keyPress(key);
 		robot.keyRelease(key);
 		int waitTime = Integer.parseInt(PropsLoader.frameworkConf("key.press.wait"));
 		int millis = TimeUtil.convertSecsToMillisWithRange(waitTime, 1, 60);
 		robot.delay(millis);
-		robot = null; // Destroy robot
 	}
 
 	/**
@@ -59,10 +56,9 @@ public final class SystemUtil {
 	 * @param keys an array of KeyEvent constants from
 	 *             {@code java.awt.event.KeyEvent}, specify keys in order
 	 * @throws AWTException
-	 * @throws NumberFormatException
 	 * @throws IOException
 	 */
-	public static void pressKeys(int[] keys) throws AWTException, NumberFormatException, IOException {
+	public static void pressKeys(int[] keys) throws AWTException, IOException {
 		Robot robot = new Robot();
 		for (int ctr = 0; ctr < keys.length; ctr++) {
 			robot.keyPress(keys[ctr]); // Press and hold keys
@@ -73,7 +69,6 @@ public final class SystemUtil {
 		for (int ctr = keys.length - 1; ctr > -1; ctr--) {
 			robot.keyRelease(keys[ctr]); // Release keys in reverse order
 		}
-		robot = null;
 	}
 
 }
