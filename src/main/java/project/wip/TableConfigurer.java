0a9886409cb13d05dc2411401a1cf0c05d7f8d38
Kat Rollo <rollo.katherine@gmail.com>
2018-09-05 19:50:51 +0800

    Add link to issue in wip

--- src/main/java/project/wip/TableConfigurer.java
+++ src/main/java/project/wip/TableConfigurer.java
@@ -11,7 +11,9 @@ import project.domain.Transaction;
 
 /**
  * Maps DataTable with label column in feature file to a single object of
- * Type&lt;T&gt;. Left column is field name, right column is value.
+ * Type&lt;T&gt;. Left column is field name, right column is value.<br>
+ * <br>
+ * Need fix for Issue <a href="https://bit.ly/2NSqn93">{@literal #1426}</a>.
  */
 public class TableConfigurer implements TypeRegistryConfigurer {
 
