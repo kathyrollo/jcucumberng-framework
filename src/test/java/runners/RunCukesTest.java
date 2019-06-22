de0879f2a1c33e9f5c408d326a495ea8715dc739
Kat Rollo <rollo.katherine@gmail.com>
2019-06-22 19:01:04 +1000

    Update to allure-cucumber4-jvm

--- src/test/java/runners/RunCukesTest.java
+++ src/test/java/runners/RunCukesTest.java
@@ -7,7 +7,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(features = { "src/main/resources/features" }, tags = { "not @ignore" }, glue = { "project.datatables",
 		"project.hooks", "project.stepdefs" }, plugin = { "pretty",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"io.qameta.allure.cucumber3jvm.AllureCucumber3Jvm", "html:target/cucumber-html-default",
+				"io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm", "html:target/cucumber-html-default",
 				"json:target/cucumber-report.json",
 				"junit:target/cucumber-report.xml" }, snippets = SnippetType.UNDERSCORE, monochrome = true, strict = true, dryRun = false)
 
