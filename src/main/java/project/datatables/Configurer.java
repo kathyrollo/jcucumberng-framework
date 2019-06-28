7b01e0f2d4b133a4efd70ec36651db80ae1d5f01
Kat Rollo <rollo.katherine@gmail.com>
2019-06-28 16:51:02 +1000

    Use Jackson mapper in datatable Configurer

--- src/main/java/project/datatables/Configurer.java
+++ src/main/java/project/datatables/Configurer.java
@@ -1,14 +1,18 @@
 package project.datatables;
 
+import java.lang.reflect.Type;
 import java.util.Locale;
 import java.util.Map;
 
 import cucumber.api.TypeRegistry;
 import cucumber.api.TypeRegistryConfigurer;
+import io.cucumber.cucumberexpressions.ParameterByTypeTransformer;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.datatable.DataTableType;
-import io.cucumber.datatable.TableEntryTransformer;
+import io.cucumber.datatable.TableCellByTypeTransformer;
+import io.cucumber.datatable.TableEntryByTypeTransformer;
 import io.cucumber.datatable.TableTransformer;
+import io.cucumber.datatable.dependency.com.fasterxml.jackson.databind.ObjectMapper;
 import project.domain.simplydo.Transaction;
 
 public class Configurer implements TypeRegistryConfigurer {
@@ -21,16 +25,10 @@ public class Configurer implements TypeRegistryConfigurer {
 	@Override
 	public void configureTypeRegistry(TypeRegistry registry) {
 
-		/*
-		 * Maps DataTable with header row to multiple objects of Type<T>. Each row below
-		 * the header is an object.
-		 */
-		registry.defineDataTableType(new DataTableType(Transaction.class, new TableEntryTransformer<Transaction>() {
-			@Override
-			public Transaction transform(Map<String, String> entry) {
-				return new Transaction(entry);
-			}
-		}));
+		JacksonTableTransformer jtt = new JacksonTableTransformer();
+		registry.setDefaultParameterTransformer(jtt);
+		registry.setDefaultDataTableCellTransformer(jtt);
+		registry.setDefaultDataTableEntryTransformer(jtt);
 
 		/*
 		 * Maps DataTable with label column to a single object of Type<T>. Left column
@@ -42,6 +40,29 @@ public class Configurer implements TypeRegistryConfigurer {
 				return new Transaction(table.asMaps().get(0));
 			}
 		}));
+
+	}
+
+	private static final class JacksonTableTransformer
+			implements ParameterByTypeTransformer, TableCellByTypeTransformer, TableEntryByTypeTransformer {
+
+		private final ObjectMapper objMapper = new ObjectMapper();
+
+		@Override
+		public Object transform(String fromValue, Type toValueType) throws Throwable {
+			return objMapper.convertValue(fromValue, objMapper.constructType(toValueType));
+		}
+
+		@Override
+		public <T> T transform(String value, Class<T> cellType) throws Throwable {
+			return objMapper.convertValue(value, cellType);
+		}
+
+		@Override
+		public <T> T transform(Map<String, String> entry, Class<T> type, TableCellByTypeTransformer cellTransformer)
+				throws Throwable {
+			return objMapper.convertValue(entry, type);
+		}
 
 	}
 
